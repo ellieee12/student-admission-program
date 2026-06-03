3c416c86be150e2193bb1a4f85ca12563e0d81a3
Gallardo Tristan <157487351+Tristan-Gallardo@users.noreply.github.com>
2026-06-03 13:23:11 +0200

    Schools bidding student part made

--- main.py
+++ main.py
@@ -1,77 +1,67 @@
-from typing import List
+from collections import defaultdict
+from typing import List, Tuple
+import pandas as pd
 
-# Check if woman prefers m over current partner m1
-def prefers(women: List[List[int]], w: int, m: int, m1: int) -> bool:
-    for i in range(len(women[w])):
-        if women[w][i] == m:
-            return True
-        if women[w][i] == m1:
-            return False
-    return False
-    
+def prefers(students: List[List[int]], stu: int, sch: int, sch1: int) -> bool:
+    return students[stu][sch] < students[stu][sch1]
 
-def stableMarriage(men: List[List[int]], women: List[List[int]]) -> List[int]:
-    n = len(men)
-
-    # women's current partners
-    w_partner = [-1] * n  
-    
-    # man's current partners
-    m_partner = [-1] * n  
-    
-    # next proposal index
-    next_proposal = [0] * n  
-    
-    # Free Men
-    free_man = [True] * n  
-
-    free_count = n
-
+def stableMarriage(schools: List[List[int]], students: List[List[int]], schools_capacity: List[int]) -> Tuple[List[set[int]], int]:
+    n = len(schools)
+    student_partner = defaultdict(lambda: None)
+    school_partner = [set() for _ in range(n)]
+    next_proposal = [0] * n
     nb_iterations = 0
+    while len(student_partner) < n :
+        if (sch := next((sch for sch in range(n) if len(school_partner[sch]) < schools_capacity[sch]), None)) is None:
+            print("there is not enough space for all students")
+            return [school_partner, nb_iterations]
+        for _ in range(schools_capacity[sch] - len(school_partner[sch])):
+            stu = schools[sch][next_proposal[sch]]
+            next_proposal[sch] += 1
+            if student_partner[stu] is None:
+                student_partner[stu] = sch
+                school_partner[sch].add(stu)
+            else:
+                sch1 = student_partner[stu]
+                if prefers(students, stu, sch, sch1):
+                    student_partner[stu] = sch
+                    school_partner[sch1].discard(stu)
+                    school_partner[sch].add(stu)
+        nb_iterations += 1
+    return [school_partner, nb_iterations]
 
-    while free_count > 0:
-        m = next((m for m in range(n) if free_man[m]), None)
+def display_assignment_table(school_partner: List[set], students: List[List[int]], nb_iteration: int):
+    rows = []
+    for sch, students_set in enumerate(school_partner):
+        for stu in sorted(students_set):
+            rows.append({
+                "Student": stu,
+                "School":  sch,
+                "Student's preference rank": students[stu][sch] + 1,
+            })
+ 
+    df_assignments = pd.DataFrame(rows).sort_values("Student").reset_index(drop=True)
+    df_assignments.index += 1
+    print(df_assignments.to_string())
+    print(f"Number of iteration: {nb_iteration}")
 
-        w = men[m][next_proposal[m]]
-        next_proposal[m] += 1
-
-        # If w is free
-        if w_partner[w] == -1:
-            w_partner[w] = m
-            m_partner[m] = w
-            free_man[m] = False
-            free_count -= 1
-        else:
-            m1 = w_partner[w]
-
-            # If w prefers m over current partner
-            if prefers(women, w, m, m1):
-                w_partner[w] = m
-                m_partner[m] = w
-
-                free_man[m] = False
-                free_man[m1] = True
-        nb_iterations=nb_iterations+1
-
-    print(nb_iterations)
-    return m_partner
 
 if __name__ == '__main__':
     n = 3
 
-    men = [
+    schools = [
         [1, 0, 2],
         [2, 1, 0],
         [0, 2, 1]
     ]
 
-    women = [
+    schools_capacity = [2, 1, 1]
+
+    students = [
         [1, 0, 2],
         [2, 1, 0],
         [0, 2, 1]
     ]
 
-    result = stableMarriage(men, women)
-
-    for i in range(n):
-        print(result[i], end=''if i!= n - 1 else '\n')
+    [result, count] = stableMarriage(schools, students, schools_capacity)
+    display_assignment_table(result, students, count)
