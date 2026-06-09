c512226b2b10aecbeacf73e999c52c44f74ee17a
Gallardo Tristan <157487351+Tristan-Gallardo@users.noreply.github.com>
2026-06-09 10:27:26 +0200

    random table generator added

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
-from typing import List, Tuple
+from typing import List, Tuple, Set
 import pandas as pd
+import random
 
 def prefers(students: List[List[int]], stu: int, sch: int, sch1: int) -> bool:
     return students[stu][sch] < students[stu][sch1]
 
-def stableMarriage(schools: List[List[int]], students: List[List[int]], schools_capacity: List[int]) -> Tuple[List[set[int]], int]:
+def stableMarriage(schools: List[List[int]], students: List[List[int]], schools_capacity: List[int]) -> Tuple[List[Set[int]], int]:
     n = len(schools)
     student_partner = defaultdict(lambda: None)
     school_partner = [set() for _ in range(n)]
@@ -14,7 +15,7 @@ def stableMarriage(schools: List[List[int]], students: List[List[int]], schools_
     while len(student_partner) < n :
         if (sch := next((sch for sch in range(n) if len(school_partner[sch]) < schools_capacity[sch]), None)) is None:
             print("there is not enough space for all students")
-            return [school_partner, nb_iterations]
+            return school_partner, nb_iterations
         for _ in range(schools_capacity[sch] - len(school_partner[sch])):
             stu = schools[sch][next_proposal[sch]]
             next_proposal[sch] += 1
@@ -28,9 +29,9 @@ def stableMarriage(schools: List[List[int]], students: List[List[int]], schools_
                     school_partner[sch1].discard(stu)
                     school_partner[sch].add(stu)
         nb_iterations += 1
-    return [school_partner, nb_iterations]
+    return school_partner, nb_iterations
 
-def display_assignment_table(school_partner: List[set], students: List[List[int]], nb_iteration: int):
+def display_assignment_table(school_partner: List[Set], students: List[List[int]], nb_iteration: int):
     rows = []
     for sch, students_set in enumerate(school_partner):
         for stu in sorted(students_set):
@@ -46,23 +47,26 @@ def display_assignment_table(school_partner: List[set], students: List[List[int]
     print(f"Number of iteration: {nb_iteration}")
 
 
+def generate_tables(n, m):
+    table1 = []
+    for _ in range(n):
+        row = list(range(m))
+        random.shuffle(row)
+        table1.append(row)
+
+    table2 = []
+    for _ in range(m):
+        row = list(range(n))
+        random.shuffle(row)
+        table2.append(row)
+
+    return table1, table2
+
+
 if __name__ == '__main__':
-    n = 3
+    schools, students = generate_tables(9, 10)
 
-    schools = [
-        [1, 0, 2],
-        [2, 1, 0],
-        [0, 2, 1]
-    ]
+    schools_capacity = [1, 1, 1, 1, 1, 1, 1, 1, 1]
 
-    schools_capacity = [2, 1, 1]
-
-    students = [
-        [1, 0, 2],
-        [2, 1, 0],
-        [0, 2, 1]
-    ]
-
-    [result, count] = stableMarriage(schools, students, schools_capacity)
-    print(result)
+    result, count = stableMarriage(schools, students, schools_capacity)
     display_assignment_table(result, students, count)
